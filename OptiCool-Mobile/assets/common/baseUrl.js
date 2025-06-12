2a7a26286a76f22ccee3ab61ac5ae23c40ace2b2
armangracia <graciaarmansteven@gmail.com>
2025-06-12 18:52:44 +0800

    minor update

--- OptiCool-Mobile/assets/common/baseUrl.js
+++ OptiCool-Mobile/assets/common/baseUrl.js
@@ -4,8 +4,8 @@ let baseURL = '';
 
 {
     Platform.OS == 'android'
-        ? baseURL = 'http://192.168.48.74:4000/api/v1'
-        : baseURL = 'http://192.168.48.74:4000/api/v1'
+        ? baseURL = 'http://192.168.100.7:4000/api/v1'
+        : baseURL = 'http://192.168.100.7:4000/api/v1'
 }
 
     
