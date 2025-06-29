2ea85a3a4c145e6c3d70c8b721400974ddd21976
armangracia <graciaarmansteven@gmail.com>
2025-06-30 01:19:51 +0800

    User, Admin, Superadmin

--- OptiCool-Mobile/navigators/BottomTabs.js
+++ OptiCool-Mobile/navigators/BottomTabs.js
@@ -56,7 +56,7 @@ export default function BottomTabs() {
         tabBarInactiveTintColor: "#7a7a7a",
       }}
     >
-      {user.role === "superadmin" || user.role === "admin" ? (
+      {user.role === "user" || user.role === "admin" ? (
         <>
           <Tab.Screen
             name="Dashboard"
